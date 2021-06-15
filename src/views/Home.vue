0e5ef565286a63d3036dae720ad0f90a940d1f39
camenzid <dario.camenzind@roche.com>
2021-06-15 20:54:01 +0200

    continue on carousel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <h1 class="text-center mt-5">Welcome to TutorialHub</h1>
+    <v-row justify="center" class="mt-3">
+      <v-col cols="11" md="8" xl="6">
+        <v-card max-width="800">
+          <v-carousel :cycle="true" class="carousel">
+            <v-carousel-item v-for="(tutorial, index) in tutorials" :key="index" class="justify-center">
+              <FImage :tutorial="tutorial" :sizes="false" :alt="'Thumbnail for Tutorial: ' + tutorial.name"></FImage>
+            </v-carousel-item>
+          </v-carousel>
+        </v-card>
+      </v-col>
+    </v-row>
     <p><!--About Section here--></p>
-    <v-carousel>
-      <v-carousel-item v-for="(tutorial, index) in tutorials" :key="index" class="justify-center">
-        <FImage class="carousel-image" :tutorial="tutorial" :sizes="false" :alt="'Thumbnail for Tutorial: ' + tutorial.name"></FImage>
-      </v-carousel-item>
-    </v-carousel>
   </div>
 </template>
 
@@ -23,8 +29,8 @@
 </script>
 
 <style>
-.v-carousel img{
+.carousel img{
   height: 100%;
-  width: auto;
+  width: 100%;
 }
 </style>
